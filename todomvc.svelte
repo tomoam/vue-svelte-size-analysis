908580c6be16ab591ab710484513cfa5ed51b6b4
tomoam <29677552+tomoam@users.noreply.github.com>
2021-07-14 18:29:50 +0900

    reflect optimization in vue to svelte

--- todomvc.svelte
+++ todomvc.svelte
@@ -2,40 +2,42 @@
 	const ENTER_KEY = 13;
 	const ESCAPE_KEY = 27;
 
-	let currentFilter = 'all';
-	let items = [];
-	let editing = null;
-
-	try {
-		items = JSON.parse(localStorage.getItem('todos-svelte')) || [];
-	} catch (err) {
-		items = [];
+	const STORAGE_KEY = 'todos-svelte';
+	const filters = {
+	  all: (todos) => todos,
+	  active: (todos) => todos.filter((todo) => !todo.completed),
+	  completed: (todos) => todos.filter((todo) => todo.completed)
 	}
 
+	let currentFilter = 'all';
+	let items = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+	let editing = null;
+
 	const updateView = () => {
-		currentFilter = 'all';
-		if (window.location.hash === '#/active') {
-			currentFilter = 'active';
-		} else if (window.location.hash === '#/completed') {
-			currentFilter = 'completed';
-		}
+		const route = window.location.hash.replace(/#\/?/, '')
+  		if (filters[route]) {
+		  currentFilter = route;
+  		} else {
+		  currentFilter = 'all';
+  		}
 	};
 
 	window.addEventListener('hashchange', updateView);
 	updateView();
 
 	function clearCompleted() {
-		items = items.filter(item => !item.completed);
+		items = filters.active(items);
 	}
 
 	function remove(index) {
-		items = items.slice(0, index).concat(items.slice(index + 1));
+		items.splice(index, 1)
+		items = items;
 	}
 
 	function toggleAll(event) {
 		items = items.map(item => ({
 			id: item.id,
-			description: item.description,
+			title: item.title,
 			completed: event.target.checked
 		}));
 	}
@@ -44,7 +46,7 @@
 		if (event.which === ENTER_KEY) {
 			items = items.concat({
 				id: Date.now(),
-				description: event.target.value,
+				title: event.target.value,
 				completed: false
 			});
 			event.target.value = '';
@@ -57,24 +59,15 @@
 	}
 
 	function submit(event) {
-		items[editing].description = event.target.value;
+		items[editing].title = event.target.value;
 		editing = null;
 	}
-	
-	$: filtered = currentFilter === 'all'
-		? items
-		: currentFilter === 'completed'
-			? items.filter(item => item.completed)
-			: items.filter(item => !item.completed);
 
-	$: numActive = items.filter(item => !item.completed).length;
-	$: numCompleted = items.filter(item => item.completed).length;
+    $: filtered = filters[currentFilter](items);
+	$: numActive = filters.active(items).length;
+	$: numCompleted = filters.completed(items).length;
 
-	$: try {
-		localStorage.setItem('todos-svelte', JSON.stringify(items));
-	} catch (err) {
-		// noop
-	}
+	$: localStorage.setItem(STORAGE_KEY, JSON.stringify(items));
 </script>
 
 <header class="header">
@@ -87,50 +80,45 @@
 	>
 </header>
 
-{#if items.length > 0}
-	<section class="main">
-		<input id="toggle-all" class="toggle-all" type="checkbox" on:change={toggleAll} checked="{numCompleted === items.length}">
-		<label for="toggle-all">Mark all as complete</label>
+<section class="main" style={items.length > 0 ? "" : "display: none;"}>
+	<input id="toggle-all" class="toggle-all" type="checkbox" on:change={toggleAll} checked="{numCompleted === items.length}">
+	<label for="toggle-all">Mark all as complete</label>
 
-		<ul class="todo-list">
-			{#each filtered as item, index (item.id)}
-				<li class="{item.completed ? 'completed' : ''} {editing === index ? 'editing' : ''}">
-					<div class="view">
-						<input class="toggle" type="checkbox" bind:checked={item.completed}>
-						<label on:dblclick="{() => editing = index}">{item.description}</label>
-						<button on:click="{() => remove(index)}" class="destroy"></button>
-					</div>
+	<ul class="todo-list">
+		{#each filtered as item, index (item.id)}
+			<li class="{item.completed ? 'completed' : ''} {editing === index ? 'editing' : ''}">
+				<div class="view">
+					<input class="toggle" type="checkbox" bind:checked={item.completed}>
+					<label on:dblclick="{() => editing = index}">{item.title}</label>
+					<button on:click="{() => remove(index)}" class="destroy"></button>
+				</div>
 
-					{#if editing === index}
-						<input
-							value='{item.description}'
-							id="edit"
-							class="edit"
-							on:keydown={handleEdit}
-							on:blur={submit}
-							autofocus
-						>
-					{/if}
-				</li>
-			{/each}
+				{#if editing === index}
+					<input
+						value='{item.title}'
+						class="edit"
+						on:keydown={handleEdit}
+						on:blur={submit}
+						autofocus
+					>
+				{/if}
+			</li>
+		{/each}
+	</ul>
+
+	<footer class="footer">
+		<span class="todo-count">
+			<strong>{numActive}</strong> {numActive === 1 ? 'item' : 'items'} left
+		</span>
+
+		<ul class="filters">
+			<li><a class="{currentFilter === 'all' ? 'selected' : ''}" href="#/">All</a></li>
+			<li><a class="{currentFilter === 'active' ? 'selected' : ''}" href="#/active">Active</a></li>
+			<li><a class="{currentFilter === 'completed' ? 'selected' : ''}" href="#/completed">Completed</a></li>
 		</ul>
 
-		<footer class="footer">
-			<span class="todo-count">
-				<strong>{numActive}</strong> {numActive === 1 ? 'item' : 'items'} left
-			</span>
-
-			<ul class="filters">
-				<li><a class="{currentFilter === 'all' ? 'selected' : ''}" href="#/">All</a></li>
-				<li><a class="{currentFilter === 'active' ? 'selected' : ''}" href="#/active">Active</a></li>
-				<li><a class="{currentFilter === 'completed' ? 'selected' : ''}" href="#/completed">Completed</a></li>
-			</ul>
-
-			{#if numCompleted}
-				<button class="clear-completed" on:click={clearCompleted}>
-					Clear completed
-				</button>
-			{/if}
-		</footer>
-	</section>
-{/if}
+		<button class="clear-completed" on:click={clearCompleted} style={numCompleted ? "" : "display:none;"}>
+			Clear completed
+		</button>
+	</footer>
+</section>
